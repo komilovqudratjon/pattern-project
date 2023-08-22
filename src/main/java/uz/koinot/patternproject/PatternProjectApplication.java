6dffadb789a174feb7ff2843884f16e2ddf5c838
Ikromov.Oybek <oybek.ikromov@ssd.uz>
2023-08-22 21:59:30 +0500

    start

---
+++ src/main/java/uz/koinot/patternproject/PatternProjectApplication.java
@@ -0,0 +1,13 @@
+package uz.koinot.patternproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/koinot/patternproject/PatternProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.koinot.patternproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
